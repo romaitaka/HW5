43d81deef53b9c55f86f977a53e6c2f164497fc2
romaitaka <trade.interes@gmail.com>
2023-11-12 18:11:50 +0300

    + Slider

---
+++ js/slider.js
@@ -0,0 +1,49 @@
+const left = document.getElementById('arrow-left'),
+    right = document.getElementById('arrow-right'),
+    slides = document.querySelectorAll('.slide'),
+    dots = document.querySelectorAll('.dot'),
+    slidesWrap = document.querySelectorAll('.slider-wrapper');
+
+let index = 0;
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+
+const activeDot = n => {
+    for (dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active');
+}
+
+const prepareCurrentSlide = ind => {
+    activeSlide(ind);
+    activeDot(ind);
+}
+
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+};
+const prevSlide = () => {
+    if (index == 0) {
+        index = slides.length - 1;
+        prepareCurrentSlide(index);
+    } else {
+        index--;
+        prepareCurrentSlide(index);
+    }
+};
+
+right.addEventListener('click', nextSlide);
+left.addEventListener('click', prevSlide);
+
